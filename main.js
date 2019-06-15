9a7b7749c1740a91e9e9508076bcd33de2b8eb92
msakuta <masahiro.sakuta@gmail.com>
2019-06-15 23:23:35 +0900

    Experiments on Electron

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 var electron = require('electron');
 var app = electron.app;
 var BrowserWindow = electron.BrowserWindow;
+const { ipcMain } = require('electron')
 
 let mainWindow = null;
 
@@ -16,11 +17,33 @@ app.on('window-all-closed', function() {
 app.on('ready', function() {
   mainWindow = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
   });
   mainWindow.loadURL('file://' + __dirname + '/MathQuiz.html');
+
+  mainWindow.webContents.openDevTools();
 
   mainWindow.on('closed', function() {
     mainWindow = null;
   });
+  
 });
+
+ipcMain.on('asynchronous-message', (event, arg) => {
+  const { dialog } = require('electron')
+  let file = dialog.showOpenDialog({ properties: ['openFile', 'openDirectory', 'multiSelections'] })
+  console.log(file)
+  console.log(arg) // prints "ping"
+  event.reply('asynchronous-reply', file)
+})
+
+ipcMain.on('synchronous-message', (event, arg) => {
+  const { dialog } = require('electron')
+  let file = dialog.showOpenDialog({ properties: ['openFile'] })
+  console.log(file)
+  console.log(arg) // prints "ping"
+  event.returnValue = file;
+})
